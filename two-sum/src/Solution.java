e37070f25295d66c13d4fe5d4ac777ab944f2f3c
bartholomewu <ronfung95@gmail.com>
2022-04-23 20:03:54 -0400

    two-sum solution

---
+++ two-sum/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        int key;
+        for (int i = 0; i < nums.length; i++){
+            key = target - nums[i];
+            if (hashMap.containsKey(key)){
+                return new int[]{hashMap.get(key), i};
+            }
+            hashMap.put(nums[i], i);
+        }
+        return null;
+    }
+
+}
