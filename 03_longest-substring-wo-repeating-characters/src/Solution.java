9bf3c193a0e773084345ba18ecebdb8ebe7f75b5
bartholomewu <ronfung95@gmail.com>
2022-04-25 23:22:44 -0400

    made windowing easier to read

--- 03_longest-substring-wo-repeating-characters/src/Solution.java
+++ 03_longest-substring-wo-repeating-characters/src/Solution.java
@@ -1,15 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length()==0) return 0;
+        if (s.length() == 0) return 0;
         HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
-        int max=0;
-        for (int i=0, j=0; i<s.length(); ++i){
-            if (hashMap.containsKey(s.charAt(i))){
-                j = Math.max(j, hashMap.get(s.charAt(i))+1);
+
+        int answer = 0;
+        for (int left = 0, right = 0; right < s.length(); right++){
+            if (hashMap.containsKey(s.charAt(right))) {
+                left = Math.max(left, hashMap.get(s.charAt(right)) + 1);
             }
-            hashMap.put(s.charAt(i),i);
-            max = Math.max(max,i-j+1);
+            hashMap.put(s.charAt(right), right);
+            answer = answer < right - left + 1 ? right - left + 1 : answer;
         }
-        return max;
+        return answer;
     }
 }
