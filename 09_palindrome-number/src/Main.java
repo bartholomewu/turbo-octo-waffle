8a43dc7ba3535cfe4c2227e5e634eac7e30a0793
bartholomewu <ronfung95@gmail.com>
2022-04-27 19:41:37 -0400

    09 palindrome

---
+++ 09_palindrome-number/src/Main.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int ans = 0;
+        int num = x;
+        while (num > 0) {
+            ans = ans * 10 + num % 10;
+            num = num / 10;
+        }
+        return ans == x;
+    }
+}
