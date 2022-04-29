a3e77c9d7ac03dcc8b2c6217253bddd86868b760
bartholomewu <ronfung95@gmail.com>
2022-04-29 11:45:11 -0400

    matrix

--- 07_reverse-integer/Solution.java
+++ 07_reverse-integer/Solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public int reverse(int x) {
         int result = 0;
-        
+
         while (x != 0) {
             int tail = x % 10;
             int r = (result * 10) + tail;
-            if ((r - tail) / 10 != result) return 0;
+            if (result != (r - tail) / 10) return 0;
+            x /= 10;
             result = r;
-            x = x/10;
         }
+
         return result;
     }
 }
